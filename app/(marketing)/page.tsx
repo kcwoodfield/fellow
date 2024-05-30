47992602bc3dc131605c90a0271d3e0ba055ddd4
Kevin Woodfield <kcwoodfield@gmail.com>
2024-05-30 12:44:09 -0500

    add global height, add marketing landing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,15 @@
+import { Medal } from 'lucide-react';
+
+const MarketingPage = () => {
+    return (
+      <div className="flex items-center justify-center flex-col">
+        <div className="flex items-center justify-center flex-col">
+          <div>
+            <Medal size="large" />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  export default MarketingPage;
